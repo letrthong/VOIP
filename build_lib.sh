49704978bc6d39942fa52fd6588a4cf6ff9f6483
Thong LT <ThongLeTrung@harveynash.vn>
2016-06-01 11:36:51 -0500

    build ortp

--- build_lib.sh
+++ build_lib.sh
@@ -1,8 +1,10 @@
-#sudo apt-get install cmake
+#https://cmake.org/download/
+#curl -sSL https://cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz | sudo tar -xzC  ./
 
 #http://blog.cleverelephant.ca/2014/12/building-cunit-from-source.html
 
 PREFIX=/home/test/VOIP
+ROOT=/home/test/VOIP
 cd ./CUnit-2.1-2
 libtoolize -f -c -i 
 aclocal 
@@ -10,8 +12,40 @@ autoconf
 automake --gnu --add-missing 
 
 ./configure  --prefix=${PREFIX} \
-	     --libdir=${PREFIX}/usr/lib/ \
-	     --includedir=${PREFIX}/usr/include/ 
+	     --libdir=${PREFIX}/opt/lib/ \
+	     --includedir=${PREFIX}/opt/include/ 
 make
 make install
+make clean
+
+cd ${ROOT}
+cd ./mbedtls
+cmake . -DCMAKE_INSTALL_PREFIX=${PREFIX}/opt \
+	-DCMAKE_PREFIX_PATH=${PREFIX}/opt
+make
+make install
+make clean
+cd ${ROOT}
+cd ./bctoolbox
+cmake . -DCMAKE_INSTALL_PREFIX=${PREFIX}/opt \
+	-DCMAKE_PREFIX_PATH=${PREFIX}/opt \
+	 -DENABLE_POLARSSL=OFF -DENABLE_MBEDTLS=ON  
+make
+make install
+make clean
+
+cd ${ROOT}
+cd ortp
+./autogen.sh
+
+cd ${ROOT}
+cd ./ortp
+./autogen.sh
+
+export BCTOOLBOX_LIBS=-L ${PREFIX}/usr/lib -lbctoolbox
+export BCTOOLBOX_CFLAGS=-I${PREFIX}/usr/include/
+./configure  --prefix=${PREFIX} \
+	     --libdir=${PREFIX}/opt/lib/ \
+	     --includedir=${PREFIX}/opt/include/ 
+
 
