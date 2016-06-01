2cd8b91b49ed93f9e09d31a7073b504e0cf1f8f7
Thong LT <ThongLeTrung@harveynash.vn>
2016-06-01 14:32:31 -0500

    add sample

--- test/start.sh
+++ test/start.sh
@@ -1,10 +1,10 @@
 export LD_LIBRARY_PATH=../out/lib/
 #rm -f out.mp3  
 echo "receive file"
-./recv "./out.mp3" 8888&
-sleep 3
+./recv out.mp3 8888&
+sleep 1
 
 
 echo "send file"
-./send "in.mp3" 192.168.159.140 8888
+./send send.wav 127.0.0.1 8888
 
