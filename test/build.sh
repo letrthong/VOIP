bf7c54e028d46dc6e00690b8e66debf7b56c01f4
Thong LT <letrthong@gmail.com>
2016-06-13 23:27:01 +0700

    remove space

--- test/build.sh
+++ test/build.sh
@@ -1,3 +1,3 @@
-g++ -o recv  -I../out/include/ rtprecv.c -L../out/lib/ -lortp  -lmbedcrypto -lbctoolbox
+g++ -o recv  -I../out/include/ rtprecv.c -L../out/lib/  -lortp  -lmbedcrypto -lbctoolbox
 
-g++ -o send  -I../out/include/ rtpsend.c -L../out/lib/ -lortp  -lmbedcrypto -lbctoolbox
+g++ -o send  -I../out/include/ rtpsend.c -L../out/lib/  -lortp  -lmbedcrypto -lbctoolbox
